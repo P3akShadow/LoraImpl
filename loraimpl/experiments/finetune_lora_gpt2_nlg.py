50d12135a47e619e6dbf1a82daac599d980f2aba
Maximilian Süss <maximilian.emanuel.suess@gmail.com>
2025-01-10 19:35:39 +0100

    replace current checkpoint

--- loraimpl/experiments/finetune_lora_gpt2_nlg.py
+++ loraimpl/experiments/finetune_lora_gpt2_nlg.py
@@ -69,10 +69,10 @@ def run_experiment(num_epochs, model_cfg, dataset_cfg, loader_cfg, optimizer_cfg
     if cont:  # Load model from checkpoint
         if run is not None:
             print('Restoring checkpoint...')
-            run.restore('checkpoint/config.json')
-            run.restore('checkpoint/generation_config.json')
-            run.restore('checkpoint/model.safetensors.json')
-            run.restore('checkpoint/optimizer.pt')
+            run.restore('checkpoint/config.json', replace=True)
+            run.restore('checkpoint/generation_config.json', replace=True)
+            run.restore('checkpoint/model.safetensors.json', replace=True)
+            run.restore('checkpoint/optimizer.pt', replace=True)
         print('Loading model from checkpoint...')
         model = GPT2LMHeadModelLora.from_pretrained("checkpoint", local_files_only=True)
         optimizer = torch.optim.AdamW(model.parameters(), **optimizer_cfg)
