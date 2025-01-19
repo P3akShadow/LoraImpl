a158fabbb43b6231168dafbb2b4a157957c6b4a6
Maximilian Süss <maximilian.emanuel.suess@gmail.com>
2025-01-19 14:42:52 +0100

    collate fn for glue sst2, mnli, cola and stsb

--- loraimpl/data/glue.py
+++ loraimpl/data/glue.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from datasets import load_dataset
+from transformers import RobertaTokenizer
 
 
 class GLUEDataset(Dataset):
@@ -75,3 +76,90 @@ class GLUEDataset(Dataset):
             'attention_mask': encoding['attention_mask'].squeeze(0),
             'label': label
         }
+
+
+class CollateFunction:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def __call__(self, batch):
+        """Creates and encodes a batch for a list of examples depending on the task.
+        The tasks provice data in different formats (we focus on sst2, mnli, cola and stsb).
+
+        sst2 example:
+        {
+            "sentence": "hide new secretions from the parental units",
+            "label": 0,
+            "idx": 0
+        }
+        mnli example:
+        {
+          "premise": "Conceptually cream skimming has two basic dimensions - product and geography.",
+          "hypothesis": "Product and geography are what make cream skimming work.",
+          "label": 1,
+          "idx": 0
+        }
+        cola example:
+        {
+          "sentence": "Our friends won't buy this analysis, let alone the next one we propose.",
+          "label": 1,
+          "idx": 0
+        }
+        stsb example:
+        {
+          "sentence1": "A plane is taking off.",
+          "sentence2": "An air plane is taking off.",
+          "label": 5.0,
+          "idx": 0
+        }
+        """
+        text = []
+        text_pair = []
+        for example in batch:
+            if 'sentence' in example:
+                text.append(example['sentence'])
+            elif 'sentence1' in example:
+                text.append(example['sentence1'])
+            elif 'premise' in example:
+                text.append(example['premise'])
+            if 'sentence2' in example:
+                text_pair.append(example['sentence2'])
+            elif 'hypothesis' in example:
+                text_pair.append(example['hypothesis'])
+        if not text_pair:
+            text_pair = None
+
+        # Encode text and text pair
+        encoding = self.tokenizer(text, text_pair, truncation=True, padding='longest', return_tensors='pt')
+        encoding['label'] = torch.tensor([example['label'] for example in batch])
+        return encoding
+
+
+if __name__ == '__main__':
+    # The tasks provide data in different formats (we focus on sst2, mnli, cola and stsb).
+    tk = RobertaTokenizer.from_pretrained('roberta-base')
+    cfn = CollateFunction(tk)
+
+    print('Loading SST-2 dataset example...')
+    ds = load_dataset('glue', 'sst2', split='train')
+    dl = DataLoader(ds, batch_size=32, collate_fn=cfn)
+    print(next(iter(dl)))
+
+    print('Loading MNLI dataset example...')
+    ds = load_dataset('glue', 'mnli', split='train')
+    dl = DataLoader(ds, batch_size=32, collate_fn=cfn)
+    print(next(iter(dl)))
+
+    print('Loading CoLA dataset example...')
+    ds = load_dataset('glue', 'cola', split='train')
+    dl = DataLoader(ds, batch_size=32, collate_fn=cfn)
+    print(next(iter(dl)))
+
+    print('Loading STS-B dataset example...')
+    ds = load_dataset('glue', 'stsb', split='train')
+    dl = DataLoader(ds, batch_size=32, collate_fn=cfn)
+    print(next(iter(dl)))
+
+
+
